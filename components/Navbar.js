d3b465656f52e719f0940353503c99158556b27e
web3Codess <vercelploy@gmail.com>
2022-08-30 21:05:16 +0300

    ..

--- components/Navbar.js
+++ components/Navbar.js
@@ -44,7 +44,7 @@ export default function Navbar() {
         <nav className="flex items-center justify-between w-10/12  lg:w-11/12 m-auto">
             <Link href={`/`} passHref><a className="flex items-center"><Image src={`https://raw.githubusercontent.com/thirdweb-dev/typescript-sdk/main/logo.svg`} alt='thirdweb/logo' width={45} height={43} priority decoding="async" objectFit="contain" /> <span className="text-2xl text-slate-300 font-bold ml-2">NFTs</span></a></Link>
 
-            {!address ? (
+            {address ? (
               <ul className="flex items-center">
                 <button type="button" className="text-lg text-blue-500 ring-2 ring-blue-600 rounded-lg px-5 py-1.5 hover:bg-blue-600 hover:text-slate-300 transition-all active:scale-90 shadow-lg hover:shadow-blue-600 md:text-sm md:px-4 md:py-1" onClick={connectMetaMask}>Connect Wallet</button>
               </ul>

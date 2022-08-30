c2b21824d24a6d1e9f685acdbe5f8a3e11d95dfc
web3Codess <vercelploy@gmail.com>
2022-08-30 20:40:03 +0300

    ..

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Navbar/>
-      {!address ? <Connect/> : <Dashboard/>}
+      {address ? <Connect/> : <Dashboard/>}
       {/* <Footer/> */}
     </>
   );
